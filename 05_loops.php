270cc84dce087e89e9e7719a01c27b9112f64cc5
erickoukos <erickoukos@gmail.com>
2022-07-25 10:29:27 +0300

    Updated Loops and Function Files

--- 05_loops.php
+++ 05_loops.php
@@ -10,3 +10,50 @@
   // code to be executed
   }
 */
+
+
+
+/* ------------ While Loop ------------ */
+
+/*
+** While Loop Syntax
+  while (condition) {
+  // code to be executed
+  }
+*/
+
+
+
+
+/* ---------- Do While Loop --------- */
+
+/*
+** Do While Loop Syntax
+  do {
+  // code to be executed
+  } while (condition);
+
+do...while loop will always execute the block of code once, even if the condition is false.
+*/
+
+
+
+/* ---------- Foreach Loop ---------- */
+
+/*
+** Foreach Loop Syntax
+  foreach ($array as $value) {
+  // code to be executed
+  }
+*/
+
+// Loop through an array
+
+
+
+// Use the indexes within the loop
+
+
+
+// Use the keys within the loop for an associative array
+
--- 06_functions.php
+++ 06_functions.php
@@ -9,3 +9,27 @@
 
   - Functions have their own local scope as opposed to global scope
 */
+
+
+
+// Running a function
+
+
+// Adding params
+
+
+// Pass in an argument
+
+
+// Returning values
+
+
+// Adding default values
+
+
+
+
+// Assigning anonymous functions to variables. Often used for closures and callback functions
+
+// Arrow functions
+
