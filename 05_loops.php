d6224ef386d74af463da652002bca413489a416f
linda500 <maestrolinda50@gmail.com>
2022-07-25 12:30:38 +0300

    modified existing files

--- 05_loops.php
+++ 05_loops.php
@@ -57,3 +57,62 @@ do...while loop will always execute the block of code once, even if the conditio
 
 // Use the keys within the loop for an associative array
 
+// working with for loop
+
+for($x = 0; $x <= 10; $x++){
+  echo " Number: $x <br/>";
+}
+
+
+// working with while loop
+echo '####### WHILE #####';
+echo '<br/>';
+$y = 1;
+while ($y <= 5){
+  echo "Number: $y <br/>"; $y++;
+}
+
+
+// working with do while loop
+echo '###### DO WHILE ##### <br/>';
+
+$a = 1;
+do {
+  echo "Number: $a <br/>";
+  $a++;
+}while ($a <= 10);
+
+echo '######### FOR EACH LOOP #########';
+
+$fruits = ['Mangoes','Apples', 'Oranges','Kiwi','Apricot'];
+echo '<br/>';
+foreach ($fruits as $fruit){
+  echo "fruits: $fruit <br/>";
+}
+
+// using indexes within loops
+echo '######## AS INDEX ####### <br>';
+
+$posts = ['Post one', 'Post two', 'Post three'];
+
+foreach ($posts as $post ){
+  echo "${posts} * ${post} <br/>";
+}
+
+
+// use keys for an associative array
+
+echo '######## ASSOCIATIVE ARRAY ######';
+echo '<br/>';
+echo '<br/>';
+
+$person =[
+  'first_name' => 'John',
+  'last_name' => 'Smith',
+  'email' => 'johnsmith@gmail.com'
+];
+
+// get the keys
+foreach($person as $key => $val){
+  echo "${key} - ${$val} <br/> ";
+}
--- 06_functions.php
+++ 06_functions.php
@@ -10,26 +10,62 @@
   - Functions have their own local scope as opposed to global scope
 */
 
+// {
+// echo "<h4 style = color: 'green';> User has been registered! </h4>";
+// }
+// register_user();
 
 
-// Running a function
+
+// function multiply  ($num1, $num2){
+//   return $num1 * $num2;
+// }
+
+// $result =multiply (12,10);
+// echo $result;
+
+// Running/calling/invoking a function
 
 
-// Adding params
+echo'<br/>';
 
+// Adding args
+function multiply($num1, $num2){
+  return $num1 * $num2;
+}
 
-// Pass in an argument
+// corresponding params
+// pass in params
+$result = multiply (12,10);
+echo $result;
 
 
 // Returning values
 
 
 // Adding default values
-
+echo '<br/>';
+function modulus($b =10, $c = 3){
+  return $b % $c;
+}
+echo modulus();
 
 
 
 // Assigning anonymous functions to variables. Often used for closures and callback functions
 
-// Arrow functions
+$var = function ($x) {
+  return pow ($x,3);
+};
 
+echo "cube of 3 =" . $var (3);
+
+// Arrow functions
+echo '<br/>';
+echo '###### ARROW FUNCTIONS ######';
+
+echo '<br/>';
+echo '<br/>';
+
+$subtract = fn($n, $m) => $n - $m;
+echo $subtract (8,5);
