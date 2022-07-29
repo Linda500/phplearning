9006a056fcf79e434433ba43ec323f7a1c294946
erickoukos <erickoukos@gmail.com>
2022-07-29 10:40:40 +0300

    modified file

--- 08_string_functions.php
+++ 08_string_functions.php
@@ -5,3 +5,9 @@
   Functions to work with strings
   https://www.php.net/manual/en/ref.strings.php
 */
+// substr() -returns a potion of a string
+
+
+// strlen() - returns the legnth of a string
+
+//
